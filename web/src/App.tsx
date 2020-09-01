600be37bc667b7bdfff3357929803e78d9d5f540
jennysol <jennyssoliver@gmail.com>
2020-08-31 21:10:24 -0300

    Configurando GlobalStyles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>Hello World</h1>
+import GlobalStyles from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <h1>Hello World</h1>
+    <GlobalStyles/>
+  </>
+);
 
 export default App;
