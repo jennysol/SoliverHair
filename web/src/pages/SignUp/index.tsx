f73f4372709f4461885d3721f31dff360812fe6e
jennysol <jennyssoliver@gmail.com>
2020-09-02 17:11:29 -0300

    Mostrando error no input

--- web/src/pages/SignUp/index.tsx
+++ web/src/pages/SignUp/index.tsx
@@ -1,7 +1,10 @@
-import React ,{ useCallback } from 'react';
+import React ,{ useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.png';
 
@@ -11,8 +14,12 @@ import Button from '../../components/Button';
 import {Container, Content, Background} from './styles';
 
 const SignUp: React.FC = () => {
-   const handleSubmit = useCallback(async (data: object) =>{
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) =>{
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
@@ -25,8 +32,14 @@ const SignUp: React.FC = () => {
       await schema.validate(data, {
         abortEarly: false,
       });
+
+
     } catch (err) {
       console.log(err);
+
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -36,7 +49,7 @@ const SignUp: React.FC = () => {
     <Content>
       <img className="logo" src={logoImg} alt="SoliverHerSalon"/>
 
-      <Form onSubmit={handleSubmit}>
+      <Form  ref={formRef} onSubmit={handleSubmit}>
         <h1>Faça seu cadastro</h1>
 
         <Input name="name" icon={FiUser} placeholder="Nome" />
