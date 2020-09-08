a05787493aac3e5c721dc62602dff0c037e80d86
jennysol <jennyssoliver@gmail.com>
2020-09-08 17:19:53 -0300

    Adicionando animaçoes no  toasts

--- web/src/components/ToastContainer/Toast/styles.ts
+++ web/src/components/ToastContainer/Toast/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css }from 'styled-components';
+import { animated } from 'react-spring';
 
 interface ContainerProps {
   type?: 'success' | 'error' | 'info';
@@ -20,14 +21,14 @@ const toastTypeVariations = {
   `,
 };
 
-export const Container = styled.div<ContainerProps>`
-width: 360px;
-position: relative;
-padding: 16px 30px 16px 16px;
-border-radius: 10px;
-box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2);
+export const Container = styled(animated.div)<ContainerProps>`
+  width: 360px;
+  position: relative;
+  padding: 16px 30px 16px 16px;
+  border-radius: 10px;
+  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2);
 
-display: flex;
+  display: flex;
 
 & + div {
   margin-top: 8px;
